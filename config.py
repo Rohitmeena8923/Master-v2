84702a1d49603a34a9078ef7bcd855313279ce79
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-04-28 10:03:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("20114039"))
-    API_HASH = os.environ.get("87297b8f3cc8fc9bbce591ad30da5896")
+    BOT_TOKEN = os.environ.get("7012934382:AAHd7-DqrVkFYzWdIxkarwC3KnYNVwrKy3A")
+    API_ID = int(os.environ.get("27775431"))
+    API_HASH = os.environ.get("b70bb1d45a1d05236671d4cc615e40f9")
     VIP_USER = os.environ.get('VIP_USERS', '').split(',')
-    VIP_USERS = [int(8172163893) for user_id in VIP_USER]
+    VIP_USERS = [int(6414266397) for user_id in VIP_USER]
